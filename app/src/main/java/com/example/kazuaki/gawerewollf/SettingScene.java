b39bd1d80a1210390110a35adf17410e7d91a16e
chubflat <khasebe1115@gmail.com>
2016-01-21 02:06:21 +0900

    LINE投稿テスト実装

--- app/src/main/java/com/example/kazuaki/gawerewollf/SettingScene.java
+++ app/src/main/java/com/example/kazuaki/gawerewollf/SettingScene.java
@@ -1,12 +1,15 @@
 package com.example.kazuaki.gawerewollf;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import static android.widget.LinearLayout.*;
 
@@ -43,6 +46,7 @@ public class SettingScene extends Activity {
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                sendTextToLine(SettingScene.this, "こちらのコメントはLINEへ投稿しました！");
                 scene = "game_scene";
                 Intent intent = new Intent(SettingScene.this,GameScene.class);
                 startActivity(intent);
@@ -74,4 +78,25 @@ public class SettingScene extends Activity {
 //        return super.onTouchEvent(event);
 //
 //    }
+
+    public static boolean appInstalled(Context context, String uri)
+    {
+        PackageManager pm = context.getPackageManager();
+        boolean appInstalled = false;
+        try{
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        }catch (PackageManager.NameNotFoundException e){
+            return false;
+        }
+    }
+    public static void sendTextToLine(Context context, String lineComment){
+        try {
+            String lineString = "line://msg/text/" + lineComment;
+            Intent intent = Intent.parseUri(lineString, Intent.URI_INTENT_SCHEME);
+            context.startActivity(intent);
+        } catch (Exception e) {
+            //LINE投稿失敗
+        }
+    }
 }
